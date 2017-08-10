434005d1e6eeb962df5cfd38a6b2ed6d715e9ae9
Sam <swkane@indiana.edu>
2017-08-10 15:05:17 -0400

    Search results populate based on search

---
+++ main.js
@@ -0,0 +1,50 @@
+let results = document.querySelector('.results');
+let searchButton = document.querySelector("button");
+let searchBox = document.querySelector('.search-input');
+
+
+let base = "https://itunes.apple.com/search?term=";
+
+
+searchButton.addEventListener("click", submitRequest);
+
+function submitRequest() {
+  let query = searchBox.value;
+  console.log(`This was your query: ${query}`);
+  let url = `${base}${query}`;
+  console.log(`This was your ${url}`);
+  fetch(url).then(function (response) {
+    response.json().then(getSongInfo)
+  });
+
+  function getSongInfo(data) {
+    console.log(`Here is the data: `,data);
+    for (var i = 0; i < 25; i++) {
+      let div = document.createElement('div');
+      div.setAttribute("class", "result");
+      div.innerHTML = `
+        <img src=${data.results[i].artworkUrl100} alt="album cover">
+        <p class="song-title">${data.results[i].trackName}</p>
+        <p class="artist-name">${data.results[i].artistName}</p>
+      `;
+      results.appendChild(div);
+    }
+  }
+}
+
+// grab the search param from .search-input
+
+// convert the typed text to encoded url
+
+//execute fetch with baseURL + translated param
+
+//append results in .results with a template literal
+
+// Single Param Example
+// https://itunes.apple.com/search?term=jack+johnson
+
+// Double Param Example
+// https://itunes.apple.com/search?term=jack+johnson&limit=25
+
+// Key value pairs are separated with an &
+// Spaces are separated with +
